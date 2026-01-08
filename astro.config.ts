e5bb03c8004853750f1a4b3a311dc161f9e22a4d
Luke Taylor <luke@deentaylor.com>
2026-01-07 20:12:08 -0500

    package updates

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import { generateAPIReferenceItems, stainlessDocs } from "@stainless-api/docs";
-import aiChat from '@stainless-api/docs-ai-chat/plugin';
+import aiChat from "@stainless-api/docs-ai-chat/plugin";
 
 // https://astro.build/config
 export default defineConfig({
